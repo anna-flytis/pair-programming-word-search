3d805c48308c08fab71e5b422ed46c70203d474f
Anna Mariakhina <77463611+anna-flytis@users.noreply.github.com>
2023-04-12 19:25:04 -0600

    Add files via upload
    word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,36 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = function(matrix) {
 
-module.exports = wordSearch
+  let array = [];
+
+  for (let i = 0; i < matrix[0].length; i++) {
+    array.push([]);
+
+  };
+
+  for (let r = 0; r < matrix.length; r++) {
+    for (let c = 0; c < matrix[r].length; c++) {
+      array[c][r] = matrix[r][c];
+    }
+  }
+
+  return array;
+};
+
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  const transposedLetters = transpose(letters).map(ls => ls.join(''));
+  for (let v of transposedLetters) {
+    if (v.includes(word)) return true;
+  }
+
+  return false;
+};
+
+
+
+
+module.exports = wordSearch;
